beeccfc92b9e8e76b9223d7c68ac88ab15901881
Rusty <rusty3925@gmail.com>
2025-02-22 17:18:51 -0800

    e

--- tag.py
+++ tag.py
@@ -5,6 +5,6 @@ from functools import reduce
 
 data = sys.stdin.read()
 
-c = Counter(match.group(0) for match in re.finditer(r'<ref +name="(.*?)" */?>', data))
+c = Counter(match.group(1) for match in re.finditer(r'<ref +name="(.*?)">', data))
 
 print(reduce(lambda x, y: x.replace(f'<ref name="{y}"', '<ref'), (i for i in c if c[i] > 1), data))
