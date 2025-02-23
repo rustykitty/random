5df0edfde83d9f87c31829af066a1de7dd81a81b
Rusty <rusty3925@gmail.com>
2025-02-22 17:14:11 -0800

    random python html parse script

---
+++ tag.py
@@ -0,0 +1,10 @@
+import sys
+import re
+from collections import Counter
+from functools import reduce
+
+data = sys.stdin.read()
+
+c = Counter(match.group(0) for match in re.finditer(r'<ref +name="(.*?)" */?>', data))
+
+print(reduce(lambda x, y: x.replace(f'<ref name="{y}"', '<ref'), (i for i in c if c[i] > 1), data))
