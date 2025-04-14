0ccd4d79b9825d6f5a94cb3356dde8f6a8ac7907
Rusty <rusty3925@gmail.com>
2025-04-14 18:36:17 +0000

    funni

--- uppercase_c/hello.c
+++ uppercase_c/hello.c
@@ -1,17 +1,22 @@
 // Made this lil thing on April Fools
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define FUNCTION 
 #define MAIN main
 #define ARGSLIST(...) (__VA_ARGS__)
 #define POINTER *
 #define ARRAY POINTER
-#define ARRAY_WITH_SIZE(_size) [_size]
+#define SIZE(_size) [_size]
 #define BEGIN {
 #define END }
 #define RETURN return
-#define PUTS(x) puts(x)
+#define PUTS(_s) puts(_s)
+#define PRINT(_s) PUTS(_s)
+#define FGETS(...) fgets(__VA_ARGS__)
+#define DEFINE
+#define PRINTF(...) printf(__VA_ARGS__)
 
 typedef int INTEGER;
 typedef char CHARACTER;
@@ -24,5 +29,9 @@ ARGSLIST(
 )
 BEGIN
     PUTS("Hello, world!");
+    DEFINE CHARACTER ARR SIZE(8);
+    PUTS("What is the first 8 letters of your name?");
+    FGETS(ARR, 8, stdin);
+    PRINTF("Nice to meet you %s.", ARR);
     RETURN 0;
 END FUNCTION
