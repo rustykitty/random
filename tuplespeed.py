68f1281da69bb394543f0b5c49bcd02f613c411c
Rusty <rusty3925@gmail.com>
2025-05-06 19:03:38 +0000

    fairer test

--- tuplespeed.py
+++ tuplespeed.py
@@ -3,12 +3,9 @@ from timeit import Timer
 # characters in 
 # "Python 3.12.7 (main, Oct  1 2024, 02:05:46) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin", 
 # put through a set
-chars_list = ['6', 'n', 'h', 'm', '[', '-', '0', 'C', 'd', ' ', '3', '5', 't', 
-              '1', 'c', '2', 'l', 'y', 'i', ':', ')', '.', '4', ',', 'w', 'g', 
-              'a', ']', 'r', 'o', '(', '7', '9', 'O', 'P']
+chars_list = [chr(i) for i in range (32, 127) if i % 2 == 0]
 
 chars_tuple = tuple(chars_list)
-
 
 alphanum = [chr(i) for i in range (32, 127)] # all printable ASCII characters
 
