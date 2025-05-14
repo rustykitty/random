c626ae5967147bf9964568c516516fe4bf7d6503
Rusty <rusty3925@gmail.com>
2025-05-14 18:13:32 +0000

    formatting code for attr_dict

--- attr_dict/__init__.py
+++ attr_dict/__init__.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+
 import functools 
 
 class AttrDict(dict):
     """
-    A dictionary that allows attribute access to keys.
+    A `dict` subclass that allows attribute access to keys.
     Inspired by JavaScript objects
     """
 
@@ -15,33 +17,39 @@ class AttrDict(dict):
         return f"{self.__class__.__name__}({super().__repr__()})"
 
     def __str__(self):
-        return str(self.__dict__)
+        return super().__repr__()
 
+    @functools.wraps(dict.copy)
     def copy(self):
         return AttrDict(super().copy())
-    
+
+    @functools.wraps(dict.fromkeys)
     def fromkeys(self, *args, **kwargs):
         return AttrDict(super().fromkeys(*args, **kwargs))
-    
+
     def __copy__(self):
         return AttrDict(super().__copy__())
-    
+
     def __deepcopy__(self, memo):
         return AttrDict(super().__deepcopy__(memo))
 
-    def __ior__(self, other):
-        if not isinstance(other, dict): return NotImplemented
-        self.update(other)
-        return self
-    
-    def __or__(self, other):
-        if not isinstance(other, dict): return NotImplemented
+    @classmethod
+    def _typecheck(cls, other):
+        return isinstance(other, dict) or isinstance(other, cls)
+
+    def __or__(self, other: dict | AttrDict):
+        if not self._typecheck(other): return NotImplemented
         res = AttrDict(self)
         res.update(other)
         return res
 
-    def __ror__(self, other):
-        if not isinstance(other, dict): return NotImplemented
+    def __ior__(self, other: dict | AttrDict):
+        if not self._typecheck(other): return NotImplemented
+        self.update(other)
+        return self
+
+    def __ror__(self, other: dict | AttrDict):
+        if not self._typecheck(other): return NotImplemented
         res = AttrDict(other)
         res.update(self)
         return res
